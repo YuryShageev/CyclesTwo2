1a2d6634562f8c0f5e7de5a1e13dbd3b16bcc22b
YuryShageev <schageev.yur@yandex.ru>
2022-10-09 17:28:32 +0300

    Второй блок 2-го этапа циклов

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        //пример
-        //System.out.println("Example");
-        //int salary = 65535;
-        //int total = 0;
-        //int rate = 12;
-        //int month = 1;
-        //int i = 0;
-       // for (;total < 1_000_000; i++) {
-        //    total = total + (total/100 * rate/12);
-         //   total = total + salary;
-        //    month = month + 1;
-        //    if (i % 5 == 0) {
-        //        System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
-        //    }
-        //}
-
-        //Задача 1
-        System.out.println("Task 1 and 2");
-        int moneyPerMonth = 15000;
-        int total = 0;
-        int rate = 7;
-        int month = 1;
-        int i = 0;
-        for (; total <= 12_000_000; i ++) {
-            total = total + (total/100 * rate/12);
-            total = total + moneyPerMonth;
-            month = month + 1;
-            if (i % 6 == 0) {
-                System.out.println("На " + month + " месяце сумма накоплений будет составлять " + total + " рублей");
-            }
+        System.out.println("Task 1");
+        int sum = 15000;
+        int monthNum = 1;
+        while (sum <= 12000000) {
+            sum *= 1.07;
+            System.out.printf("Месяц %d, сумма %d \n", monthNum, sum);
+            monthNum++;
         }
-
-
     }
+
+
+
 }
