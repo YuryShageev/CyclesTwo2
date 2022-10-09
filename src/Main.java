9221a40a017a5b343613b58c4a8ae616de5a81a1
YuryShageev <schageev.yur@yandex.ru>
2022-10-09 18:26:39 +0300

    Второй блок 2-го этапа циклов

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,18 @@ public class Main {
         }
 
         System.out.println("Task 3");
-        //НЕПОНЯТНО НИЧЕГО!!!
+        int sum1 = 15000;
+        for (int i = 1; i < 9 * 12; i += 6) {
+            int sumBefore = sum1;
+            sum1 *= 1 + 0.07 * 6;
+            System.out.printf("месяц %d, сумма %d \n", i, sum1 - sumBefore);
+        }
+
+        System.out.println("Task 4");
+        int firstFriday = 3;
+        for (int currentFriday = firstFriday; currentFriday <= 31; currentFriday += 7) {
+            System.out.printf("Сегодня пятница, " + currentFriday + " число." + " Необходимо подготовить отчёт! ");
+        }
 
     }
 
